66033c5a38c9818230429bcc87e39ceecf11aca3
Eugene K. (wantToHawaii) <ekaryakin@bekitzur.com>
2021-02-09 15:07:45 +0300

    Page liberica native image

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,69 +1,102 @@
-window.addEventListener('load', () => {
+import Swiper, { Pagination, EffectFade } from "swiper";
+import "swiper/swiper.scss";
+import "swiper/components/effect-fade/effect-fade.scss";
+import "swiper/components/pagination/pagination.scss";
+
+Swiper.use([Pagination, EffectFade]);
+
+window.addEventListener("load", () => {
   const initHeaderMenu = () => {
-    const header = document.querySelector('#site-header');
+    const header = document.querySelector("#site-header");
 
     if (!header) return;
 
-    const menu = header.querySelector('#header-menu');
-    const iconMenuButton = header.querySelector('#icon-menu-button');
+    const menu = header.querySelector("#header-menu");
+    const iconMenuButton = header.querySelector("#icon-menu-button");
 
-    iconMenuButton.addEventListener('click', () => {
-      menu.classList.toggle('active');
+    iconMenuButton.addEventListener("click", () => {
+      menu.classList.toggle("active");
     });
   };
-  
+
   const initFooterMenu = () => {
-    const footer = document.querySelector('#site-footer');
+    const footer = document.querySelector("#site-footer");
 
     if (!footer) return;
 
-    const toggleMenuButtons = footer ? footer.querySelectorAll('.button-toggle-menu') : null;
-    
+    const toggleMenuButtons = footer
+      ? footer.querySelectorAll(".button-toggle-menu")
+      : null;
+
     for (let button of toggleMenuButtons) {
-      button.addEventListener('click', function() {
-        this.classList.toggle('active');
+      button.addEventListener("click", function () {
+        this.classList.toggle("active");
       });
     }
   };
-  
+
   const initCardsWithDropDownText = () => {
-    const cardsWithDropDownText = document.querySelectorAll('.has-drop-down-text');
-    const toggleCardDropDownText = function(e) {
+    const cardsWithDropDownText = document.querySelectorAll(
+      ".has-drop-down-text"
+    );
+    const toggleCardDropDownText = function (e) {
       const card = this;
-      const isButton = e.target.classList.contains('toggle-drop-down-text');
+      const isButton = e.target.classList.contains("toggle-drop-down-text");
       if (isButton) {
-        card.classList.toggle('drop-down-text-visible');
+        card.classList.toggle("drop-down-text-visible");
       }
     };
     if (cardsWithDropDownText && cardsWithDropDownText.length) {
       for (let card of cardsWithDropDownText) {
-        card.addEventListener('click', toggleCardDropDownText);
+        card.addEventListener("click", toggleCardDropDownText);
       }
     }
   };
-  
+
   const onContactFormSubmit = (selector) => {
     const form = document.querySelector(selector);
-    const modal = form ? form.querySelector('.bs-callback-modal') : null;
-    
+    const modal = form ? form.querySelector(".bs-callback-modal") : null;
+
     if (!form || !modal) return;
-    
-    const closeButtons = modal.querySelectorAll('[data-close]');
-  
-    form.addEventListener('submit', (e) => {
+
+    const closeButtons = modal.querySelectorAll("[data-close]");
+
+    form.addEventListener("submit", (e) => {
       e.preventDefault();
-      form.classList.add('submitted');
+      form.classList.add("submitted");
     });
-  
+
     for (let btn of closeButtons) {
-      btn.addEventListener('click', () => {
-        form.classList.remove('submitted');
+      btn.addEventListener("click", () => {
+        form.classList.remove("submitted");
       });
     }
+  };
+
+  const initLibericaNativeImageSlider = () => {
+    const libericaNativeImageFeaturesSlider = new Swiper(
+      "#liberica-native-image-features-carousel",
+      {
+        effect: "fade",
+        pagination: {
+          el: "#liberica-native-image-features-carousel .swiper-pagination",
+          clickable: true,
+          autoHeight: true,
+        },
+        breakpoints: {
+          768: {
+            slidesPerView: 3,
+            effect: "slide",
+            direction: "horizontal",
+          },
+        },
+      }
+    );
   };
 
   initHeaderMenu();
   initFooterMenu();
   initCardsWithDropDownText();
-  onContactFormSubmit('#contact-form');
+  onContactFormSubmit("#contact-form");
+  initLibericaNativeImageSlider();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,50 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     port: 3001,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
   },
-  devtool: 'inline-source-map',
-  mode: 'development',
-  entry: [
-    './src/scripts/index.js',
-    './src/styles/styles.scss',
-  ],
+  devtool: "inline-source-map",
+  mode: "development",
+  entry: ["./src/scripts/index.js", "./src/styles/styles.scss"],
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'styles.css',
+      filename: "styles.css",
     }),
-    new HtmlWebpackPlugin({ filename: 'index.html', template: './src/index.html' }),
-    new HtmlWebpackPlugin({ filename: 'high-powered-support.html', template: './src/high-powered-support.html' }),
-    new HtmlWebpackPlugin({ filename: 'road-map.html', template: './src/road-map.html' }),
-    new HtmlWebpackPlugin({ filename: 'pricing.html', template: './src/pricing.html' }),
-    new HtmlWebpackPlugin({ filename: 'components.html', template: './src/components.html' }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "high-powered-support.html",
+      template: "./src/high-powered-support.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "road-map.html",
+      template: "./src/road-map.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "pricing.html",
+      template: "./src/pricing.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "liberica-native-image.html",
+      template: "./src/liberica-native-image.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "components.html",
+      template: "./src/components.html",
+    }),
   ],
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
@@ -36,29 +52,29 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime']
-          }
-        }
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
-          {loader: MiniCssExtractPlugin.loader},
-          'css-loader',
-          'sass-loader'
+          { loader: MiniCssExtractPlugin.loader },
+          "css-loader",
+          "sass-loader",
         ],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: 'file-loader',
+        use: "file-loader",
       },
       {
         test: /\.html$/,
-        loader: 'html-loader'
+        loader: "html-loader",
       },
-    ]
-  }
+    ],
+  },
 };
