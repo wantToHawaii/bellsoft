cb9c65469b53185c35656c01e65e324300f5140c
Eugene K. (wantToHawaii) <ekaryakin@bekitzur.com>
2021-07-25 21:52:23 +0300

    Add animation onScroll for review cards

--- src/scripts/page-libericajdk-vs-openjdk.js
+++ src/scripts/page-libericajdk-vs-openjdk.js
@@ -1,19 +1,41 @@
 window.addEventListener('load', () => {
-  const changeAnchorsMenuBackgroundOnScroll = () => {
-    const anchorsMenu = document.querySelector('#page-libericajdk-vs-openjdk-anchors');
-    const sectionHero = document.querySelector('#page-libericajdk-vs-openjdk-hero')
-    if (anchorsMenu && sectionHero) {
-      window.addEventListener('scroll', (e) => {
-        if (window.scrollY >= sectionHero.clientHeight) {
-          if (!anchorsMenu.classList.contains('blue')) {
-            anchorsMenu.classList.add('blue');
-          }
-        } else if (anchorsMenu.classList.contains('blue')) {
-          anchorsMenu.classList.remove('blue');
-        }
-      });
+  if (!document.querySelector('.page-libericajdk-vs-openjdk')) return;
+
+  const anchorsMenu = document.querySelector('#page-libericajdk-vs-openjdk-anchors');
+  const sectionHero = document.querySelector('#page-libericajdk-vs-openjdk-hero')
+
+  const reviewCard1 = document.querySelector('.bs-card-review.one');
+  const reviewCard2 = document.querySelector('.bs-card-review.two');
+
+  const changeAnchorsMenuBackgroundOnScroll = (menu, hero) => {
+    if (window.scrollY >= hero.clientHeight) {
+      if (!menu.classList.contains('blue')) {
+        menu.classList.add('blue');
+      }
+    } else if (menu.classList.contains('blue')) {
+      menu.classList.remove('blue');
     }
   };
 
-  changeAnchorsMenuBackgroundOnScroll();
+  const addClassToReviewCardsOnScroll = (...cards) => {
+    const isInView = (el) => {
+      return (el.getBoundingClientRect().top - window.innerHeight) <= 0;
+    }
+    const manageClass = (el) => {
+      if (!el.classList.contains('animate')) {
+        el.classList.add('animate');
+      }
+    }
+    cards.forEach(item => {
+      if (isInView(item)) manageClass(item);
+    })
+  };
+
+  changeAnchorsMenuBackgroundOnScroll(anchorsMenu, sectionHero)
+  addClassToReviewCardsOnScroll(reviewCard1, reviewCard2);
+
+  window.addEventListener('scroll', () => {
+    changeAnchorsMenuBackgroundOnScroll(anchorsMenu, sectionHero)
+    addClassToReviewCardsOnScroll(reviewCard1, reviewCard2);
+  });
 });
